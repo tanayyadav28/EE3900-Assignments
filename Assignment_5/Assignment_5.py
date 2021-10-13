6985a884b21b4c063b1343ecc062175f6c87db4a
tanayyadav28 <80532306+tanayyadav28@users.noreply.github.com>
2021-10-13 20:54:50 +0530

    Update Assignment_5.py

--- Assignment_5/Assignment_5.py
+++ Assignment_5/Assignment_5.py
@@ -5,27 +5,29 @@ len = 1000
 x_axis = np.array([0]*len)
 
 # standard parabola
-X = np.linspace(-10, 10, len)
+X = np.linspace(-5, 5, len)
 y = np.power(X, 2)
-
+s_par = X**2
 Y = np.vstack((X, y))
 
 # parameters of the given parabola
 V = np.array([[1, 0], [0, 0]])
-u = np.array([0.5, 0])
+u = np.array([0.5, -0.5])
 f = 1
 
 # Affine transformation
-g = -np.array([0, 1])
+g = -0.5*np.array([0, 1])
 vcm = g - u
 vcp = g + u
 A = np.vstack((V, vcp.T))
 b = np.append(vcm, -f)
 c = np.linalg.lstsq(A, b, rcond=None)[0]
 c = c.reshape(2, 1)
-
 # Generating the parabola
 x_par = Y + c
+
+# plotting the standard parabola
+plt.plot(X, s_par, label='standard parabola', color='green')
 
 # plotting the parabola
 plt.plot(x_par[0, :], x_par[1, :], label='$x^2 + x + 1$')
@@ -33,6 +35,7 @@ plt.plot(x_par[0, :], x_par[1, :], label='$x^2 + x + 1$')
 # Plotting all points
 plt.plot(c[0], c[1], 'o', color='red')
 plt.plot(X, x_axis, label='x axis', color='black')
+
 plt.text(c[0] * (1 - 1.5), c[1] * (1 + 1), '$(-0.5 , 0.75)$')
 plt.ylim(-2, 10)
 plt.xlabel('$x$')
